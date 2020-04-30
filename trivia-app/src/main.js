a4a3b743d54ac4220dd46a842e3bb2f1dab7a36a
Ryan Ho <ryanjho90@gmail.com>
2020-04-30 16:04:01 +0800

    Displaying question and choices - working

--- trivia-app/src/main.js
+++ trivia-app/src/main.js
@@ -1,5 +1,23 @@
 console.log('Hello world');
 
 $( () => {
-    
+
+    $.ajax({
+        url: 'https://opentdb.com/api.php?amount=10'
+    }).then(data => {
+        console.log(data.results[0]);
+
+        $('#question').text(data.results[0].question)
+        const choices = [];
+        choices.push(data.results[0].correct_answer);
+        data.results[0].incorrect_answers.forEach(choice => {
+            choices.push(choice);
+        })
+        
+        while (choices.length > 0 ) {
+            const choice = choices.splice(Math.floor(Math.random() * choices.length), 1);
+            $('.choices').append($('<p>').text(choice));
+        }
+        
+    })
 })
