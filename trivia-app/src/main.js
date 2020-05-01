4fcc22f4e580909897e2d3f1f1679fd610534426
Ryan Ho <ryanjho90@gmail.com>
2020-05-01 22:27:28 +0800

    Completed Basic Game Play

--- trivia-app/src/main.js
+++ trivia-app/src/main.js
@@ -1,35 +1,65 @@
-const triviaQuestions = [];
+let questions = [];
+let currentQuestion;
+let score = 0, questionCounter = 0;
 
-const loadTriviaQuestions = array => {
-    array.forEach(element => {
-        triviaQuestions.push(element);
-    })
-};
+const loadQuestions = array => {
+    questions = [...array];
+}
+
+const getQuestion = array => {
+    if (questions.length === 0) return $(location).attr('href', './result.html')
+    questionCounter++;
+    console.log(questionCounter);
+    $('.options-section').empty();
+
+    const randomIndex = Math.floor(Math.random() * array.length) - 1;
+    currentQuestion =  array.splice(randomIndex, 1)[0];
+
+    displayQuestionText(currentQuestion);
+    displayQuestionOptions(currentQuestion);
+    
+}
+
+const displayQuestionText = (question) => {
+    $('#question').text(question.question);
+}
+
+const checkAnswer = (question, response) => {
+    const correctAnswer = question.correct_answer === response;
+    if (correctAnswer) question++;
+    return correctAnswer;
+}
+
+const displayQuestionOptions = (question) => {
+    let options = [...question.incorrect_answers];
+    const randomIndex  = Math.floor(Math.random() * question.length) -1;
+    options.splice(randomIndex, 0, question.correct_answer);
+    
+    for (let i = 0; i < options.length; i++) {
+        const $optionsSection = $('.options-section');
+        const $optionContainer = $('<div>').addClass('option-container').html(`<p class='option-number'>${i+1}</p><p class='option-text'>${options[i]}</p>`);
+        $optionsSection.append($optionContainer);
+
+        $optionContainer.on('click', event => {
+
+            const $response = $(event.currentTarget).children().eq(1).text();
+            console.log(checkAnswer(question, $response));
+            getQuestion(questions);
+        })
+
+    }
+}
+
+const startGame = (data) => {
+    questionCounter = 0;
+    loadQuestions(data);
+    getQuestion(questions);
+}
 
 $( () => {
-
     $.ajax({
         url: 'https://opentdb.com/api.php?amount=10'
     }).then(data => {
-        
-        const randomNumber = Math.floor(Math.random() * data.results.length);
-        const options = [];
-
-        const selectedTriviaObject = data.results[randomNumber];
-        $('#question').text(selectedTriviaObject.question);
-        
-        console.log(selectedTriviaObject);
-
-        options.push(selectedTriviaObject.correct_answer);
-        selectedTriviaObject.incorrect_answers.forEach(option => {
-            options.push(option);
-        })
-
-        const iterations = options.length;        
-        for (let i = 0; i < iterations; i++) {
-            const option = options.splice(Math.floor(Math.random() * options.length), 1);
-            $('.options-section').append($('<div>').addClass('option-container').html(`<p class='option-number'>${i+1}</p><p class='option-text'>${option}</p>`));
-        }
-        
+        startGame(data.results);
     })
 })
