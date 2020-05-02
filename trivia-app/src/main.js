3681d5e665ab09367c425b4a5acd9645deed5105
Ryan Ho <ryanjho90@gmail.com>
2020-05-02 09:57:49 +0800

    Added feature: Game Information (Question Counter, Score)

--- trivia-app/src/main.js
+++ trivia-app/src/main.js
@@ -1,38 +1,31 @@
 let questions = [];
+let numberQuestions = 0;
 let currentQuestion;
 let score = 0, questionCounter = 0;
 
 const loadQuestions = array => {
     questions = [...array];
+    numberQuestions = questions.length;
 }
 
-const getQuestion = array => {
-    if (questions.length === 0) return $(location).attr('href', './result.html')
-    questionCounter++;
-    console.log(questionCounter);
-    $('.options-section').empty();
-
-    const randomIndex = Math.floor(Math.random() * array.length) - 1;
-    currentQuestion =  array.splice(randomIndex, 1)[0];
-
-    displayQuestionText(currentQuestion);
-    displayQuestionOptions(currentQuestion);
-    
+const displayGameInformation = () => {
+    $('#question-number').text(`${questionCounter} / ${numberQuestions}`)
+    $('#score').text(`${score}`);
 }
 
 const displayQuestionText = (question) => {
     $('#question').text(question.question);
 }
 
-const checkAnswer = (question, response) => {
-    const correctAnswer = question.correct_answer === response;
+const checkAnswer = (question, userChoice) => {
+    const correctAnswer = question.correct_answer === userChoice;
     if (correctAnswer) question++;
     return correctAnswer;
 }
 
 const displayQuestionOptions = (question) => {
     let options = [...question.incorrect_answers];
-    const randomIndex  = Math.floor(Math.random() * question.length) -1;
+    const randomIndex  = Math.floor(Math.random() * questions.length);
     options.splice(randomIndex, 0, question.correct_answer);
     
     for (let i = 0; i < options.length; i++) {
@@ -41,13 +34,42 @@ const displayQuestionOptions = (question) => {
         $optionsSection.append($optionContainer);
 
         $optionContainer.on('click', event => {
+            const userChoice = $(event.currentTarget).children().eq(1).text();
+            
+            if (checkAnswer(question, userChoice)) {
+                score++;
+                $(event.currentTarget).addClass('correct-option');    
+            } else {
+                $(event.currentTarget).addClass('wrong-option');
+                
+                $('.option-text').each( index => {
+                    if ($(`.option-text:eq(${index})`).text() === question.correct_answer) setTimeout( () => {
+                        $(`.option-text:eq(${index})`).parent().addClass('correct-option');
+                    }, 500);
+                })
+            }
 
-            const $response = $(event.currentTarget).children().eq(1).text();
-            console.log(checkAnswer(question, $response));
-            getQuestion(questions);
+            setTimeout( () => {
+                getQuestion(questions);
+            }, 2000)
+
+            
         })
 
     }
+}
+
+const getQuestion = array => {
+    if (questions.length === 0) return $(location).attr('href', './result.html')
+    questionCounter++;
+    displayGameInformation();
+    $('.options-section').empty();
+
+    const randomIndex = Math.floor(Math.random() * array.length) - 1;
+    currentQuestion =  array.splice(randomIndex, 1)[0];
+
+    displayQuestionText(currentQuestion);
+    displayQuestionOptions(currentQuestion);
 }
 
 const startGame = (data) => {
@@ -58,8 +80,11 @@ const startGame = (data) => {
 
 $( () => {
     $.ajax({
-        url: 'https://opentdb.com/api.php?amount=10'
+        url: 'https://opentdb.com/api.php?amount=15'
     }).then(data => {
         startGame(data.results);
+    }).catch(error => {
+        console.log("ERROR MESSAGE BELOW:")
+        console.log(error);
     })
 })
